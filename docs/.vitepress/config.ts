4cb47141ef318b4beb296fe1c8106857e6e1f9c9
lyusis <lyusislyu@gmail.com>
2024-02-06 17:29:06 +0800

    feature:清理门户

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Lyusis Blog",
   description: "Lyu's Blog",
+  base: './',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
